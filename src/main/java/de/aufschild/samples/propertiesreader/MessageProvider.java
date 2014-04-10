58aac416d6acfb027448f9a1a7358ed0ab651c00
Volker <mail@volker-aufschild.de>
2014-04-10 22:07:48 +0200

    with injected @Value

--- src/main/java/de/aufschild/samples/propertiesreader/MessageProvider.java
+++ src/main/java/de/aufschild/samples/propertiesreader/MessageProvider.java
@@ -3,6 +3,7 @@ package de.aufschild.samples.propertiesreader;
 import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,10 +11,12 @@ public class MessageProvider {
 
 	@Autowired
 	private Properties messages;
-
+	@Value("default")
+	private String defaultValue;
+	
 	public String getProp(String key) {
 		String val = messages.getProperty(key);
-		return val == null ? "<empty>" : val;
+		return val == null ? defaultValue : val;
 	}
 	
 	
